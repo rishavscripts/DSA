5172b1d55029a82712c1cce8a04a3b1ef23f8afc
rishavscripts <mitrarishav2004@gmail.com>
2026-05-23 21:37:50 +0530

    Pattern21 done

---
+++ Strivers A2Z DSA Course/Patterns/pattern21.java
@@ -0,0 +1,24 @@
+package Patterns;
+import java.util.*;
+public class pattern21 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Enter the value of n: ");
+        int n= sc.nextInt();
+        pattern21(n);
+    }
+    public static void pattern21(int n){
+        for(int i=0;i<n;i++){
+            if(i==0||i==(n-1)){
+                for(int j=0;j<n;j++){System.out.print("*");}
+                System.out.println();
+            }
+            else{
+                System.out.print("*");
+                for(int k=0;k<n-2;k++){System.out.print(" ");}
+                System.out.print("*");
+                System.out.println();
+            }
+        }
+    }
+}
