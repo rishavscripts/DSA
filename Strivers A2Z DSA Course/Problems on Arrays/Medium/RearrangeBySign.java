3018996ae673accf111869168de14671fe80cb63
rishavscripts <mitrarishav2004@gmail.com>
2026-05-30 12:27:46 +0530

    Rearrange By Sign done

--- Strivers A2Z DSA Course/Problems on Arrays/Medium/RearrangeBySign.java
+++ Strivers A2Z DSA Course/Problems on Arrays/Medium/RearrangeBySign.java
@@ -1,33 +1,27 @@
-// error in this code
+
+import java.util.*;
 public class RearrangeBySign {
     public static void main(String[] args){
-        int[] arr={2, 4, 5, -1, -3, -4};
+        int[] arr={1, -1, -3, -4, 2, 3};
         int[] ans = rearrange(arr);
         for(int n:ans){System.out.print(n+" ");}
     }    
     public static int[] rearrange(int[] arr){
-        //int[] ans=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>0){
-                for(int j=i+1;j<arr.length;j++){
-                    if(arr[j]<0){
-                        int temp=arr[i];
-                        arr[i]=arr[j];
-                        arr[j]=temp;
-                    }
-                }
-            }
-            else{
-                for(int j=i+1;j<arr.length;j++){
-                    if(arr[j]>0){
-                        int temp=arr[i];
-                        arr[i]=arr[j];
-                        arr[j]=temp;
-                    }
-                }
-            }
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        for(int n: arr){
+            if(n<0){neg.add(n);}
+            else{pos.add(n);}
         }
-               
-        return arr;
+        int[] ans = new int[arr.length];
+        if(arr[0]>=0){
+            for(int i=0;i<ans.length;i=i+2){ans[i]=pos.getFirst(); pos.removeFirst();}
+            for(int i=1;i<ans.length;i=i+2){ans[i]=neg.getFirst(); neg.removeFirst();}
+        }
+        else{
+            for(int i=0;i<ans.length;i=i+2){ans[i]=neg.getFirst(); neg.removeFirst();}
+            for(int i=1;i<ans.length;i=i+2){ans[i]=pos.getFirst(); pos.removeFirst();}
+        }
+        return ans;
     }
 }
