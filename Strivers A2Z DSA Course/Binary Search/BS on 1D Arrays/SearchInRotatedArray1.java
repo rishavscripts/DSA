11d32c9b4e0e857816974f16b4b1e9a04f510de5
rishavscripts <mitrarishav2004@gmail.com>
2026-06-03 21:01:56 +0530

    Search in rotated Array done

---
+++ Strivers A2Z DSA Course/Binary Search/BS on 1D Arrays/SearchInRotatedArray1.java
@@ -0,0 +1,14 @@
+public class SearchInRotatedArray1 {
+    public static void main(String[] args){
+        int[] arr={4, 5, 6, 7, 0, 1, 2};
+        int target=0;
+        int ans=search(arr,target);
+        System.out.println(ans);
+    }    
+    public static int search(int[] arr, int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target) return i;
+        }
+        return -1;
+    }
+}
