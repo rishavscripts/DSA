b4f9064c79d3a3a3504d475613f68804e2a9d9f7
rishavscripts <mitrarishav2004@gmail.com>
2026-06-25 22:41:42 +0530

    Chef fantasy 11 done

---
+++ CodeChef/Rating 500 to 1000/ChefFantasy11.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ChefFantasy11 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n=sc.nextInt();
+            System.out.println(n*(n-1));
+        }
+    }
+}
