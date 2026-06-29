a65fb6a243b5b1f40a98ba2c030b3f1b18bd94d8
rishavscripts <mitrarishav2004@gmail.com>
2026-06-29 21:16:19 +0530

    ChefInOffice done

---
+++ CodeChef/Rating 500 to 1000/ChefInOffice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ChefInOffice {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            System.out.println((x*4)+y);
+        }
+    }
+}
