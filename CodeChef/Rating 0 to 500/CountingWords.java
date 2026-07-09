5b0620a72fa4c1ffac2cb1d2d7dce3419dc60c45
rishavscripts <mitrarishav2004@gmail.com>
2026-07-09 22:20:55 +0530

    CountingWords done

---
+++ CodeChef/Rating 0 to 500/CountingWords.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CountingWords {
+    static Scanner sc = new Scanner(System.in);
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        
+        int t=sc.nextInt();
+        while(t-->0){
+            int n= sc.nextInt();
+            int m=sc.nextInt();
+            System.out.println(n*m);
+        }
+	}
+}
