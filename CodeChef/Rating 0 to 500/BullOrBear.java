e5ca438a8ba8838c5b3658e96614a352b4cc7f11
rishavscripts <mitrarishav2004@gmail.com>
2026-07-10 16:06:26 +0530

    Bull or Bear done

---
+++ CodeChef/Rating 0 to 500/BullOrBear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BullOrBear {
+    static Scanner sc= new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(y>x){System.out.println("PROFIT");}
+            else if(y<x){System.out.println("LOSS");}
+            else{System.out.println("NEUTRAL");}
+        }
+	}
+}
