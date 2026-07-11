152d7e55eeeb7c0302935a3afbbb6993d4667028
rishavscripts <mitrarishav2004@gmail.com>
2026-07-11 11:03:10 +0530

    ChairsRequirement done

---
+++ CodeChef/Rating 0 to 500/ChairsRequirement.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ChairsRequirement {
+     static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(y>=x){System.out.println(0);}
+            else{System.out.println(x-y);}
+        }
+	}
+}
