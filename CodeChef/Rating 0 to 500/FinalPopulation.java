f0e4ab394e34b0d19187ce85ea9019c474c312da
rishavscripts <mitrarishav2004@gmail.com>
2026-07-15 12:13:14 +0530

    Final Population done

---
+++ CodeChef/Rating 0 to 500/FinalPopulation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class FinalPopulation {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            System.out.println(x-y+z);
+        }
+	}
+}
