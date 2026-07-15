8350e85731bdea7210c66932c6e7e9b46a200fee
rishavscripts <mitrarishav2004@gmail.com>
2026-07-15 11:54:23 +0530

    WaterRequirement done

---
+++ CodeChef/Rating 0 to 500/WaterRequirement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class WaterRequirement {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            System.out.println(n*2);
+        }
+	}    
+}
