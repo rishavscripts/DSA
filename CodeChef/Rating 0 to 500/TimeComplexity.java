d265a05c91b7ccf42f09b1a770692485dac9df44
rishavscripts <mitrarishav2004@gmail.com>
2026-07-15 12:21:45 +0530

    TimeComplexity done

---
+++ CodeChef/Rating 0 to 500/TimeComplexity.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class TimeComplexity {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(x>y){System.out.println("YES");}
+            else{System.out.println("NO");}
+        }
+	}
+}
