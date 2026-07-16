56ab698b85cf6bb4041e45eaf1c57a31b832d4e7
rishavscripts <mitrarishav2004@gmail.com>
2026-07-16 09:57:50 +0530

    MinimumCoins done

---
+++ CodeChef/Rating 0 to 500/MinimumCoins.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MinimumCoins {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+           int n=sc.nextInt();
+            System.out.println(n%10);
+        }
+	}
+}
