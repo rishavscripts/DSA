9cbbbfa1367b351b65ae7f143ed79b894f055c4f
rishavscripts <mitrarishav2004@gmail.com>
2026-07-18 19:09:24 +0530

    HeightOfRationals done

---
+++ CodeChef/Rating 0 to 500/HeightOfRationals.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HeightOfRationals {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(Math.max(a,b));
+	}
+}
