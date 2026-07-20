063dc39ea6b8ed7a96a1af1aaa3c297d2bcc09dd
rishavscripts <mitrarishav2004@gmail.com>
2026-07-20 21:22:34 +0530

    CreditScore done

---
+++ CodeChef/Rating 0 to 500/CreditScore.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CreditScore {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        if(t>=750){System.out.println("YES");}
+        else{System.out.println("NO");}
+	}
+}
