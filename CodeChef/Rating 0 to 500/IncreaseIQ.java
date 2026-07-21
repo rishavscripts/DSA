d4ec9c5a1b6dd252ad16affa552c307438c35563
rishavscripts <mitrarishav2004@gmail.com>
2026-07-21 22:38:36 +0530

    IncreaseIQ done

---
+++ CodeChef/Rating 0 to 500/IncreaseIQ.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class IncreaseIQ {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int x=sc.nextInt();
+        if(x+7>170){System.out.println("YES");}
+        else{System.out.println("NO");}
+	}
+}
