4338d21d9ba3466244405f36cc7677ab7518a993
rishavscripts <mitrarishav2004@gmail.com>
2026-07-22 21:48:16 +0530

    DominantArmy done

---
+++ CodeChef/Rating 0 to 500/DominantArmy.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DominantArmy {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            
+            if(a>b+c){System.out.println("YES");}
+            else if(b>a+c){System.out.println("YES");}
+            else if(c>b+a){System.out.println("YES");}
+            else{System.out.println("NO");}
+        }
+	}
+}
