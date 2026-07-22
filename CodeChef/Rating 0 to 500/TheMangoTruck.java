1731a57875e5446c4f141da0ea9eb4f0df423d7e
rishavscripts <mitrarishav2004@gmail.com>
2026-07-22 11:47:28 +0530

    TheMangoTruck done

---
+++ CodeChef/Rating 0 to 500/TheMangoTruck.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TheMangoTruck {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            
+            System.out.println((z-y)/x);
+        }
+	}
+}
+
