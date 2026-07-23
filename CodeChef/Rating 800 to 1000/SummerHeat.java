a0bc3548bf1d7432d4456a9cd2eeffe13aa7387b
rishavscripts <mitrarishav2004@gmail.com>
2026-07-23 09:27:41 +0530

    SummerHeat done

---
+++ CodeChef/Rating 800 to 1000/SummerHeat.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class SummerHeat {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        int t=sc.nextInt();
+        while(t-->0){
+            int xa=sc.nextInt();
+            int xb=sc.nextInt();
+            int Xa=sc.nextInt();
+            int Xb=sc.nextInt();
+
+            System.out.println(Xa/xa+Xb/xb);
+        }
+    }
+}
