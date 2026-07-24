cb0106227027ee466f2bf3de549d53c7e9255be8
rishavscripts <mitrarishav2004@gmail.com>
2026-07-24 21:41:36 +0530

    Adjacent Sum Parity done

--- CodeChef/Rating 0 to 500/Minimum_Coins.java
+++ CodeChef/Rating 0 to 500/Minimum_Coins.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MinimumCoins {
+public class Minimum_Coins {
     static Scanner sc = new Scanner(System.in);
 	public static void main (String[] args) throws java.lang.Exception
 	{
---
+++ CodeChef/Rating 1000 to 1400/AdjacentSumParity.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AdjacentSumParity {
+    static Scanner sc=new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){arr[i]=sc.nextInt();}
+            int flag=0;
+            for(int i=0;i<n;i++){flag=flag^arr[i];}
+            if(flag==0){System.out.println("YES");}
+            else{System.out.println("NO");}
+        }
+	}
+}
