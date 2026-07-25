99dc5c1a117e16bd23904d5cc515e6e7c1d2df78
rishavscripts <mitrarishav2004@gmail.com>
2026-07-25 19:47:55 +0530

    GroupAssignment done

--- CodeChef/Rating 800 to 1000/GroupAssignment.java
+++ CodeChef/Rating 800 to 1000/GroupAssignment.java
@@ -4,7 +4,9 @@ public class GroupAssignment {
     public static void main(String[] args) {
         int t=sc.nextInt();
         while(t-->0){
-            
+            int n =sc.nextInt();
+            int x=sc.nextInt();
+            System.out.println(2*n-x+1);
         }
     }
 }
