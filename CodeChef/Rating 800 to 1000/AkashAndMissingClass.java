5aa77a167488bba12c4cbd7db85e082f1f48419c
rishavscripts <mitrarishav2004@gmail.com>
2026-07-30 00:00:37 +0530

    AkashAndMissingClass done

---
+++ CodeChef/Rating 800 to 1000/AkashAndMissingClass.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AkashAndMissingClass {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            System.out.println(n/6);
+        }
+    }
+}
