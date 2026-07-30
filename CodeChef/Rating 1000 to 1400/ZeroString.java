af087888cb767addb786e31912e16c7b8ee95883
rishavscripts <mitrarishav2004@gmail.com>
2026-07-30 10:57:15 +0530

    ZeroString done

--- CodeChef/Rating 1000 to 1400/ZeroString.java
+++ CodeChef/Rating 1000 to 1400/ZeroString.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 
-public class PrimeReversal {
+public class ZeroString {
     static Scanner sc = new Scanner(System.in);
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
         int t=sc.nextInt();
         while(t-->0){
-            //int n=sc.nextInt();
-            String a=sc.next();
-            String b=sc.next();
-            int a0 = zeros(a);
-            int a1 = ones(a);
-            int b0 = zeros(b);
-            int b1 = ones(b);
-            if(a0==b0 && a1==b1){System.out.println("YES");}
-            else{System.out.println("NO");}
+            int n=sc.nextInt();
+            String s=sc.next();
+            System.out.println(check(s,n));
         }
+	}
+	public static int check(String s, int n){
+	    int s0=zeros(s);
+	    int s1=ones(s);
+	    return Math.min(s1,s0+1);
 	}
 	public static int zeros(String s){
 	    int count=0;
