0511cf68a727eca7cf2fb3f2872818728ea348aa
Rishav Mitra <mitrarishav2004@gmail.com>
2026-08-14 10:32:50 +0530

    Problem4 done

---
+++ Data Structures/Linked List/Single Linked List/Problem4.java
@@ -0,0 +1,42 @@
+// 4. Delete the first node of a singly linked list.  
+
+class Node{
+    public int data;
+    public Node next;
+    Node(){this.data=-1; this.next=null;}
+    Node(int data){this.data=data; this.next=null;}
+    Node(int data, Node next){this.data=data; this.next=next;}
+}
+public class Problem4 {
+    static Node head=null;
+     public static Node insertAtEnd(Node head, int data) {
+        Node node = new Node(data);
+        if (head == null) {return node;}
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = node;
+        return head;
+    }
+    public static void print(Node head){
+        System.out.println("The Linked List :");
+        while(head!=null){
+            System.out.println(head.data);
+            head=head.next;
+        }
+    }
+    public static void main(String[] args) {
+        // static initialization of Linked List.
+        head=insertAtEnd(head, 0);
+        head=insertAtEnd(head, 1);
+        head=insertAtEnd(head, 2);
+        // Main driver program.
+        System.out.println("Before Deleting the First Node: ");
+        print(head);
+        head=deleteFirstNode(head);
+        System.out.println("After Deleting the First Node: ");
+        print(head);
+    }
+    public static Node deleteFirstNode(Node head){
+        System.out.println("The deleted Node data : "+head.data);
