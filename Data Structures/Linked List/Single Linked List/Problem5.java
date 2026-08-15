088b5d3eb9599c1f79b2913bbdbe654875baf286
Rishav Mitra <mitrarishav2004@gmail.com>
2026-08-15 23:26:39 +0530

    Problem5 done

---
+++ Data Structures/Linked List/Single Linked List/Problem5.java
@@ -0,0 +1,42 @@
+// 5. Delete the last node of a singly linked list.
+class Node{
+    public int data;
+    public Node next;
+    Node(){this.data=-1; this.next=null;}
+    Node(int data){this.data=data; this.next=null;}
+    Node(int data, Node next){this.data=data; this.next=next;}
+}
+public class Problem5 {
+    static Node head=null;
+
+    public static void print(Node head){
+        System.out.println("The Linked List :");
+        while(head!=null){
+            System.out.println(head.data);
+            head=head.next;
+        }
+    }
+    public static Node insertAtEnd(Node head, int data) {
+        Node node = new Node(data);
+        if (head == null) {return node;}
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = node;
+        return head;
+    }
+    public static void main(String[] args) {
+        // static initialization of Linked List.
+        head=insertAtEnd(head, 0);
+        head=insertAtEnd(head, 1);
+        head=insertAtEnd(head, 2);
+        // Main driver program.
+        System.out.println("The Linked List before Deleting the last Node");
+        print(head);
+        head=deleteLastNode(head);
+        System.out.println("The Linked List after Deleting the Last Node");
+        print(head);
+    }
+    public static Node deleteLastNode(Node node){
+        if(head==null || head.next==null) return null;
