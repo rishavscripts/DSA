589bb0dae693d19d3146bd91fb9f02bf9759e315
rishavscripts <mitrarishav2004@gmail.com>
2026-08-21 10:57:02 +0530

    StickBreak done

---
+++ CodeChef/Rating 1000 to 1200/StickBreak.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StickBreak{
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int l=sc.nextInt();
+            int k=sc.nextInt();
+            if(l%k==0){System.out.println(0);}
+            else{System.out.println(1);}
+        }
+	}
+}
