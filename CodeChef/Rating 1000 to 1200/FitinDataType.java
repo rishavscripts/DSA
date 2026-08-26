197f21ff8f1c4033d6d79eb4cc78509f15a96a12
rishavscripts <mitrarishav2004@gmail.com>
2026-08-26 09:31:14 +0530

    FitinDataType done

---
+++ CodeChef/Rating 1000 to 1200/FitinDataType.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FitinDataType {
+    static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            System.out.println(x % (n + 1));
+        }
+	}
+}
