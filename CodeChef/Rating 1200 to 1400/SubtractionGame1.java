dc6b464215d0b7f280f1818f87d0421cebd3962d
rishavscripts <mitrarishav2004@gmail.com>
2026-09-06 19:32:40 +0530

    SubtractionGame1 done

---
+++ CodeChef/Rating 1200 to 1400/SubtractionGame1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class SubtractionGame1 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){arr[i]=sc.nextInt();}
+
+            int g=arr[0];
+            for(int a: arr){
+                g=gcd(g,a);
+            }
+            System.out.println(g);
+        }
+    }
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
